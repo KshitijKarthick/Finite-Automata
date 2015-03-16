b92bb5ae5c3a3c0b1b3f0b5681cba776ba27bb4d
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-03-16 20:43:49 +0530

    Exception Handling and maintaining consistency in data formats

--- Finite_Automata.py
+++ Finite_Automata.py
@@ -25,42 +25,46 @@ class DeterministicFiniteAutomata():
         # if no i/p parameters are provided, takes i/p for all parameters.
         if no_of_states == 0 and no_of_symbols == 0 and states == None and symbols == None :
 
-            print "Enter total no of states:"
-            self.no_of_states = int(raw_input())
-            print "Enter no of Alphabets valid in the Finite Automata:"
-            self.no_of_symbols = int(raw_input())
-            self.states = {}
-            self.symbols = []
-            # Symbols i/p upto no_of_symbols
-            for index in range(self.no_of_symbols):
-                print "Enter Symbol " + str(index + 1) + ":"
-                self.symbols.append(raw_input())
-            print "\nInitial State is denoted by Q0:\n"
-            # Iterate over every state for receiving transitions.
-            for state_id in range(self.no_of_states):
-                transition_list = []
-                # Iterate over every symbol transition for a particular state.
-                for symbol_id in range(self.no_of_symbols):
-                    print "Enter Q" + str(state_id) + " State Transition ID for input Symbol",self.symbols[symbol_id],":"
-                    destination_id = int(raw_input())
-                    # Invalid state data
-                    if destination_id >= self.no_of_states:
+            try:
+                print "Enter total no of states:"
+                self.no_of_states = int(raw_input())
+                print "Enter no of Alphabets valid in the Finite Automata:"
+                self.no_of_symbols = int(raw_input())
+                self.states = {}
+                self.symbols = []
+                # Symbols i/p upto no_of_symbols
+                for index in range(self.no_of_symbols):
+                    print "Enter Symbol " + str(index + 1) + ":"
+                    self.symbols.append(raw_input())
+                print "\nInitial State is denoted by Q0:\n"
+                # Iterate over every state for receiving transitions.
+                for state_id in range(self.no_of_states):
+                    transition_list = []
+                    # Iterate over every symbol transition for a particular state.
+                    for symbol_id in range(self.no_of_symbols):
+                        print "Enter Q" + str(state_id) + " State Transition ID for input Symbol",self.symbols[symbol_id],":"
+                        destination_id = int(raw_input())
+                        # Invalid state data
+                        if destination_id >= self.no_of_states:
+                            print "Entered Incorrect Data, Program will Exit"
+                            exit(-1)
+                        # Store every Transition by Index for that Symbol.
+                        transition_list.append(destination_id)
+                    # Final state or not [1 -> Final, 0 -> Non Final]
+                    print "Enter 1 if the Q"+str(state_id)+" is a Final State, else 0 for Non Final State:"
+                    final_state = int(raw_input())
+                    # Invalid i/p
+                    if final_state != 1 and final_state != 0:
                         print "Entered Incorrect Data, Program will Exit"
-                        exit(-1)
-                    # Store every Transition by Index for that Symbol.
-                    transition_list.append(destination_id)
-                # Final state or not [1 -> Final, 0 -> Non Final]
-                print "Enter 1 if the Q"+str(state_id)+" is a Final State, else 0 for Non Final State:"
-                final_state = int(raw_input())
-                # Invalid i/p
-                if final_state != 1 and final_state != 0:
-                    print "Entered Incorrect Data, Program will Exit"
-                    return -1
-                state={}
-                # Dictionary Construction {'Final':0 or 1, 'Transition:[1,2,...]'}
-                state['Final']=final_state
-                state['Transitions']=transition_list
-                self.states["Q"+str(state_id)] = state
+                        return -1
+                    state={}
+                    # Dictionary Construction {'Final':0 or 1, 'Transition:[1,2,...]'}
+                    state['Final']=final_state
+                    state['Transitions']=transition_list
+                    self.states[state_id] = state
+
+            except ValueError:
+                exit(-1)
 
         # if i/p parameters are provided.
         else:
@@ -77,15 +81,17 @@ class DeterministicFiniteAutomata():
         """
 
         # Initial Start State.
-        state = "Q0"
+        state = 0
         try:
             # Individual String to be Processed, iterated through the constructed DFA.
             for letter in string:
                 # Each symbol traverses from start state till the i/p ends.
-                state="Q" + str((self.states[state]['Transitions'])[self.symbols.index(letter)])
+                state = (self.states[state]['Transitions'])[self.symbols.index(letter)]
             # Validate last state reached to be a Final State.
             return self.states[state]['Final']
         except KeyError:
+            return -1
+        except ValueError:
             return -1
 
 if __name__ == '__main__':
