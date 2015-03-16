5d7c0e6eae57504a55f875124e0e87e49394864e
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-03-16 22:16:47 +0530

    DFA Construction, Validation support set up as a POST Request

--- server.py
+++ server.py
@@ -14,6 +14,27 @@ class Server():
         template = env.get_template('index.html')
         return template.render()
 
+    @cherrypy.expose
+    def dfa(self):
+
+        received_data = cherrypy.request.body.read()
+        decoded_data = json.loads(received_data)
+        try:
+            no_of_states = decoded_data['no_of_states']
+            no_of_symbols = decoded_data['no_of_symbols']
+            states = decoded_data['states']
+            symbols = decoded_data['symbols']
+            string = decoded_data['string']
+            temp_states = {}
+            for key in states.keys():
+                temp_states[int(key)]=states[key]
+            states = temp_states
+        except KeyError:
+            return json.dumps({"status":2, "message":"Invalid Data Sent to the Server"})
+
+        dfa = DeterministicFiniteAutomata(no_of_symbols, no_of_states, states, symbols)
+        return json.dumps(dfa.validate_dfa(string))
+
 if __name__ == '__main__':
     ''' Setting up the Server with Specified Configuration'''
 
