3e925b55b6f038399ca5d13ae47edab91a177a98
heartz <ranadip.rulz@gmail.com>
2015-06-04 18:07:08 +0530

    Added DSL input frontend

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,179 +1,167 @@
-var n_fin;
-var n_states;
-var n_alphabets;
-var alpha;
-var len;
-var st=1,al=1,alp=1,nf=1;
-var regex=/^[0-9]+$/;
-var transition_list=[];
-var valid_states=[];
-var no_fin;
-var final_states;
-var string_to_check;
-function make_table(){
-	var table=$("#trans_table");
-	var row2add="";
-	for( var y=0; y<n_states;y++)
-	{
-		for(var z=0; z<n_alphabets;z++)
-		{
-			row2add="<tr>";
-			row2add += "<td>" +"q"+y+"-----"+ "</td>";
-			row2add += "<td>"+alpha[z]+"---->"+ "</td>";
-			row2add += "<td>"+"<input id=in_"+ y + z +">"+"</input>"+ "</td>";
-			row2add += "</tr>";
-			table.append(row2add);
-		}
+//Clears the input area on load
 
-	}
-	var e=$('<button id="btn_tab" value="generate new element">submit</button>');
-	$('#div7').append(e);
+$(document).ready(function(){
+	$('#input_area').val('');
+});
+
+// Separates the input into lines
+
+function lineSpliter(lines){
+	var list_of_lines=lines.split(/ *\n */);
+	return list_of_lines;
 }
-//.one() can be used but it does not allow us to discard multiple incorrect inputs
-$('#div1').on('click','#btn_states',function(){
-	n_states=$('#no_states').val();
-	if(!n_states.match(regex))
-		alert("Please enter a number");
-		else if((st--)>0)//due to the use of op-- , once valid input is given , it cannot be changed
-		{	
-			var y;
-			var temp_state;
-			$('#btn_states').hide();
-			for(y=0;y<n_states;y++)
-			{
-				temp_state="q"+y;
-				valid_states.push(temp_state);
-			}
-			var r= $('<input type="number" id="no_alphabets" value="generate new element"/>');
-			var s=$('<button id="btn_alphabets" value="generate new element">submit</button>');
-			$('#div2').append("Enter the number of alphabets:")
-			$('#div2').append(r);
-			$('#div2').append(s);
-		}
-});
 
-$('#div2').on('click','#btn_alphabets',function(){
-	n_alphabets=$('#no_alphabets').val();
-	if(!n_alphabets.match(regex))
-		alert("please enter a number");
-	else if((al--)>0)
-	{
-		$('#btn_alphabets').hide();
-		var p= $('<input type="text" id="alphabets"/>');
-		var q=$('<button id="btn_alpha" value="generate new element">submit</button>');
-		$('#div3').append("Enter the alphabets seperated by a comma:")
-		$('#div3').append(p);
-		$('#div3').append(q);
-	}
-});	
-$('#div3').on('click','#btn_alpha',function(){
-	var temp=$('#alphabets').val();
-	alpha=temp.split(",");
-	len=alpha.length;
-	if(len!=n_alphabets)
-		alert("Please enter the proper number of alphabets");
-	else if((alp--)>0)
-	{
-		for(y=0;y<n_alphabets;y++)
-		{
-			for(z=y+1;z<n_alphabets;z++)
-				if(alpha[y]==alpha[z])
-				{
-					alert("please enter unique alphabets");
-					alp++;
-					return;
-				}
-		}
-			$('#btn_alpha').hide();
-			var t=$('<button id="btn_start" value="generate new element">click to enter transition table</button>');
-			$('#div4').append(t);
-	}
-});
+// Accepts the input lines and return an object with states and coresponding symbol
 
-//.one allows the statements following an event to occur only once 
-$('#div4').on('click','#btn_start',function(){
-	$('#btn_start').hide();
-	$('#div4').append("<br>Fill the table.<br>q0 is the initial state");
-	$('#div5').append("Enter the number of final states");
-	var m=$('<input type="number" id="no_finalstates"/>');
-	$('#div5').append(m);
-	var n=$('<button id="btn_no_fin" value="generate new element">submit</button>');
-	$('#div5').append(n);
-});
+function transitionParser(line){
+	var flag=0;
+	var initial_partitions=line.split(/ *< */);
+	if(initial_partitions.indexOf('')===-1 && initial_partitions.length===2);
+	else{
+		alert("Input syntax error");
+		return "Error";
+	}
+	var final_partitions=initial_partitions[1].split(/ *> */);
+	if(final_partitions.indexOf('')===-1 && final_partitions.length===2);
+	else{
+		alert("Input syntax error");
+		return "Error";
+	}
 
-$('#div5').on('click','#btn_no_fin',function(){
-	no_fin=$('#no_finalstates').val();
-	if(!no_fin.match(regex))
-	{
-		alert("Enter a number");
-		return;
+	var start_state=initial_partitions[0];
+	var symbol=final_partitions[0];
+	var stop_state=final_partitions[1];
+	if(symbol===''||start_state===''||stop_state===''){
+		alert("Input syntax error");
+		return "Error";
 	}
-	else if(no_fin>n_states)
-	{
-		alert("Number of final states should not be greater than the number of states");
-		return;
-	}
-	else(nf--)
-	{
-		$('#btn_no_fin').hide();
-		$('#div6').append("Enter the final states seperated by a comma");
-		var s1=$('<input type="text" id="final_states"/>');
-		var p1=$('<button id="btn_fin" value="generate new element">submit</button>');
-		$('#div6').append(s1);
-		$('#div6').append(p1);
-	}
-});
+	var transition={'start':start_state,'symbol':symbol,'stop':stop_state};
+	return transition;
+}
 
-$('#div6').on('click','#btn_fin',function(){
-	var temp1;
-	temp1=$('#final_states').val();
-	final_states=temp1.split(',');
-	var len1=final_states.length;
-	if(no_fin!=len1)
-	{
-		alert("Enter the appropriate number of final states");
-		return;
+//Function to remove duplicate objects from a list of objects
+function duplicateObjectRemover(list){
+	var arrResult = {},
+	unique = [];
+	for (i = 0, n = list.length; i < n; i++) {
+		var item = list[i];
+		//Create an array of object with key as the values of the original objects concatinated and value as the object
+		arrResult[item.start+"-"+item.symbol+ "-"+item.stop] = item;
 	}
+	i = 0;
+	for (var item in arrResult) {
+		unique[i++] = arrResult[item];
+	}
+	return unique;
+}
+
+//Function to check for ambiguity in transition
+
+function ambiguityChecker(list){
+	var arrResult = {},
+	unique = [];
+	for (i = 0, n = list.length; i < n; i++) {
+		var item = list[i];
+		//Creating object key with only start state and symbol to check for multiple entries in the transition table
+		arrResult[item.start+"-"+item.symbol] = item;
+	}
+	i = 0;
+	for (var item in arrResult) {
+		unique[i++] = arrResult[item];
+	}
+	if(list.length!==unique.length)
+		return true;
 	else
-	{
-		$('#btn_fin').hide();
-		make_table();
-	}
-});
+		return false;
+}
 
-$('#div7').on('click','#btn_tab',function(){
-	var start_state;
-	var transistion_symbol; 
-	var next_state;
-	var temp;
-	var x;
-	for( var y=0; y<n_states;y++)
-	{
-		for(var z=0; z<n_alphabets;z++)
-		{
-			start_state="q"+y;
-			transistion_symbol=alpha[z];
-			next_state=$("#in_"+y+z).val();
-			temp = {'start':start_state,'symbol':transistion_symbol,'stop':next_state};
-			transition_list.push(temp);	
-		}
+
+//Function to remove duplicates from a normal list
+function duplicateRemover(list){
+	var a=[];
+	for (var i = 0; i < list.length; i++) {
+		if(a.indexOf(list[i])===-1) a.push(list[i]);
 	}
-	for(y=0;y<transition_list.length;y++)
-		if(valid_states.indexOf(transition_list[y].stop)===-1){
-			alert("Enter valid states ");
-			transition_list=[];
+	return a;
+}
+
+// Function calls on click event of Submit Button
+
+$('#submit-button').click(function(){
+	var transition_list=[];
+	var states_list=[];
+	var symbol_list=[];
+	var statements=$('#input_area').val();
+	if(statements===''){
+		alert("Input cannot be Empty");
+		return;
+	}
+	var lines=lineSpliter(statements);
+	if(lines.length<3){
+		alert("Not enough Input");
+		return;
+	}
+	for(i=0;i<lines.length-2;i++){
+		var temp_object= transitionParser(lines[i]);
+		if(temp_object==="Error"){
+			// Clears the list is error occurs
+			transition_list=[]
 			return;
 		}
-	$('#btn_tab').hide();
-		var m1=$('<input type="text" id="inp_string"/>');
-		var n1=$('<button id="btn_input_string" value="generate new element">submit</button>');
-		$('#div8').append("Enter the string to be validated");
-		$('#div8').append(m1);
-		$('#div8').append(n1);
-	});
-	$('#div8').on('click','#btn_input_string',function(){
-		$('#btn_input_string').hide();
-		string_to_check=$('#inp_string').val();
-	var generated_json=jsonGenerator(valid_states,alpha,transition_list,final_states,string_to_check);
+		transition_list.push(temp_object);
+	}
+	// Removing duplicate transitions
+	transition_list=duplicateObjectRemover(transition_list);
+	// Checking for ambiguity in transitions
+	var ambiguity=ambiguityChecker(transition_list);
+	if(ambiguity===true){
+		alert("Cannot have different stop state for same start state and symbol");
+		return;
+	}
+	// Pushing the states and symbols in the transition table into lists
+	for(i=0;i<transition_list.length;i++){
+		states_list.push(transition_list[i].start);
+		symbol_list.push(transition_list[i].symbol);
+	}
+	for(i=0;i<transition_list.length;i++){
+		states_list.push(transition_list[i].stop);
+	}
+	// Checking for Final states input
+	var final_states_list=lines[lines.length-2].split(/ *: */)
+	if(final_states_list[0]!=='Final'){
+		alert("Final state not properly specified");
+		return;
+	}
+	final_states_list=final_states_list[1].split(/ *, */);
+	final_states_list = duplicateRemover(final_states_list);
+	// Checking for "null" final states
+	if(final_states_list.indexOf('')!==-1){
+		alert("Invalid final state input");
+		return;
+	}
+	// Pushing the states in final states to states list
+	for(i=0;i<final_states_list.length;i++){
+		states_list.push(final_states_list[i]);
+	}
+	//Checking for String input
+	var string_input=lines[lines.length-1].split(/ *: */)
+	if(string_input[0]!=='String'){
+		alert("String to be checked not properly specified");
+		return;
+	}
+	var string_to_check = string_input[1];
+	// Removing Duplicates from states and symbol list
+	states_list=duplicateRemover(states_list);
+	symbol_list=duplicateRemover(symbol_list);
+	var no_of_states = states_list.length;
+	var no_of_symbol= symbol_list.length;
+	// Verifing the data in transition table
+	if(no_of_symbol*no_of_states!=transition_list.length){
+		alert("Input data in transition table insufficient");
+		return;		
+	}
+	// Generating and validating the data
+	var generated_json=jsonGenerator(states_list,symbol_list,transition_list,final_states_list,string_to_check);
 	validator(generated_json);
+
 });
